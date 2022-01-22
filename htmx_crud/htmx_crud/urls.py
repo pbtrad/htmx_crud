94d9142000415c67b141fe4c3c84f0c7c754033c
paul <paulbrowne2@gmail.com>
2022-01-22 09:31:51 +0000

    first commit

---
+++ htmx_crud/htmx_crud/urls.py
@@ -0,0 +1,27 @@
+"""htmx_crud URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/4.0/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path
+
+from htmx_todo.views import todos, add_todo, update_todo, delete_todo
+
+urlpatterns = [
+    path('', todos, name='todos'),
+    path('add-todo/', add_todo, name='add_todo'),
+    path('update/<int:pk>/', update_todo, name='update_todo'),
+    path('delete/<int:pk>/', delete_todo, name='delete_todo'),
+    path('admin/', admin.site.urls),
+]
